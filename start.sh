649e3bf20ba70a7b35158811a49a4e8d4bacdf3e
Christian - Darker97 <christian@baltzer.de>
2020-12-26 13:00:09 +0100

    added shell script for easy deployment

---
+++ start.sh
@@ -0,0 +1,16 @@
+# Vorbereitung
+# git checkout release
+# git pull
+
+#-------------------------
+# Bau des WebpageImage
+cd Webpage
+docker build -t webpageimage:0.1 .
+
+cd ../
+# ------------------------
+# Docker compose
+echo "-----------------------------------------"
+cd ./DockerFiles
+echo "start von Docker Compose"
+docker-compose up
