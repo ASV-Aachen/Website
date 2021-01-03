572738a67a12074ca1b6c05272689889124f659c
Christian - Darker97 <35217578+Darker97@users.noreply.github.com>
2021-01-03 19:03:48 +0100

    Models für das Grundgerüst der Webpage

--- Webpage/FrontPage/models.py
+++ Webpage/FrontPage/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from Mitglieder.models import User
 
-# Create your models here.
+# Infos über das aktuelle Jahr
+class Jahresinfo(models.Model):
+    Jahr = models.IntegerField(primary_key=True)
+    ZuLeistendeArbeitsstunden = models.IntegerField()
+
+
+# Speicher für InfoSeiten welche wir gerne mal verändern
+class InfoPage(models.Model):
+    Titel = models.CharField(max_length=200, primary_key=True)
+    Text = models.TextField(null=False)
+    Beschreibung = models.TextField()
+
+
+# Modell für alle Blogeinträge
+class Blogeintrag(models.Model):
+    Titel = models.CharField(max_length=200)
+    Text = models.TextField()
+    Autor = models.ForeignKey(User)
+    Datum = models.DateField(auto_created=True)
