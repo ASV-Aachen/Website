6fd2b4987b915b6e3fa16be9852de0ac1e97fe3e
Christian - Darker97 <35217578+Darker97@users.noreply.github.com>
2021-01-12 23:12:09 +0100

    Added Admin registration

--- Webpage/FrontPage/admin.py
+++ Webpage/FrontPage/admin.py
@@ -1,10 +1,7 @@
 from django.contrib import admin
-from ..Mitglieder.models import Profile
-from ..FrontPage.models import Blogeintrag
+from .models import BlogEintrag
 
 # Register your models here.
-# personen Kontrolle
-admin.site.register(Profile)
 
 # News
-admin.site.register(Blogeintrag)
+admin.site.register(BlogEintrag)
