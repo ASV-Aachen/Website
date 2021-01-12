2666a8ed78bba3887b45e26e496840bf7c4db83a
Christian - Darker97 <35217578+Darker97@users.noreply.github.com>
2021-01-12 23:13:05 +0100

    Added First URLs

--- Webpage/Django/wsgi.py
+++ Webpage/Django/wsgi.py
@@ -8,9 +8,10 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 """
 
 import os
-
+from django.conf import settings
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Django.settings')
 
 application = get_wsgi_application()
+
--- Webpage/FrontPage/urls.py
+++ Webpage/FrontPage/urls.py
@@ -13,10 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
+
+from django.urls import path
+from . import views
+
+app_name: "FrontPage"
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('FrontPage.urls'))
+    path('', views.MainPage, name="ASV"),
+    path('login', views.loginFunction, name="login"),
+    path('News', views.EinzelNews, name="News")
 ]
