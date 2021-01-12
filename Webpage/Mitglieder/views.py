37c6c6ee5d03069d1d128f26feb3d3709ea92261
Christian - Darker97 <35217578+Darker97@users.noreply.github.com>
2021-01-12 23:12:28 +0100

    MitgliederBearbeiten Init

--- Webpage/Mitglieder/views.py
+++ Webpage/Mitglieder/views.py
@@ -1,16 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Profile
 
 # Create your views here.
 
-
 # View um die eigenen einstellungen zu bearbeiten
-
 
 # Mitgliederverzeichnis
 # TODO
 
 # Bearbeiten
 # TODO
+def MitgliedBearbeiten(request):
+    if request.user.is_authenticated():
+        if request.GET:
+            id = request.user.id
+            return render(request, template_name="MitgliedBearbeiten.html", context={'News': Profile.objects.get(id=id)})
+        if request.POST:
+            id = request.user.id
+            AktuellerNutzer = Profile.objects.get(id=id)
+
+            AktuellerNutzer.Heimatstadt = request.POST.get('Heimatstadt')
+            AktuellerNutzer.PLZ = request.POST.get("PLZ")
+            AktuellerNutzer.Land = request.POST.get("Land")
+
+            AktuellerNutzer.EMail = request.POST.get("EMail")
+            AktuellerNutzer.HandyNummer = request.POST.get("HandyNummer")
+            AktuellerNutzer.save()
+
+            return redirect("/")
+            pass
+    else:
+        return redirect("/")
+
 
 # Passwort ändern
 # TODO
