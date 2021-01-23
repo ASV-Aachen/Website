a05008db1b74ebba4d87c60d80ceb6c35983a8a6
Christian - Darker97 <christian@baltzer.de>
2021-01-23 23:51:17 +0100

    Keycloack implementiert.
    BUG: Proxy funktioniert noch nicht korrekt

--- DockerFiles/init.sql
+++ DockerFiles/init.sql
@@ -13,3 +13,11 @@ GRANT ALL PRIVILEGES ON websiteDB.* to 'website'@'%';
 CREATE USER 'Keycloack'@'%' IDENTIFIED BY 'my-secret-pw';
 CREATE DATABASE KeycloackDB;
 GRANT ALL PRIVILEGES ON KeycloackDB.* TO 'Keycloack'@'%';
+
+
+-- for SSO
+SET GLOBAL innodb_file_format = Barracuda;
+SET GLOBAL innodb_file_per_table = on;
+SET GLOBAL innodb_default_row_format = dynamic;
+SET GLOBAL innodb_large_prefix = 1;
+SET GLOBAL innodb_file_format_max = Barracuda;
