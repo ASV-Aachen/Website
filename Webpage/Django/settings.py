a7734873c6405b4e8fc3d767d2b40369fca06d41
Christian - Darker97 <christian@baltzer.de>
2021-01-24 23:36:22 +0100

    Erster Versuch Keycloack einzubauen

--- Webpage/Django/settings.py
+++ Webpage/Django/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'FrontPage',
     'Mitglieder',
     # 'django.contrib.sites',
+    'django_keycloak.apps.KeycloakAppConfig',
 ]
 
 MIDDLEWARE = [
@@ -50,6 +51,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django_keycloak.middleware.BaseKeycloakMiddleware',
+]
+
+AUTHENTICATION_BACKENDS = [
+    'django_keycloak.auth.backends.KeycloakAuthorizationCodeBackend',
 ]
 
 ROOT_URLCONF = 'Django.urls'
--- Webpage/Django/urls.py
+++ Webpage/Django/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('FrontPage.urls'))
+    path('', include('FrontPage.urls')),
+    path('keycloak/', include('django_keycloak.urls')),
 ]
