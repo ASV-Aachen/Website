957b1edbac0e9e96dc707791f7429a21bfca613e
Patrick Oberherr <patrick.oberherr@gmail.com>
2021-01-27 19:56:43 +0100

    Happy moving

---
+++ bin/start.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# Zum root verzeichnis wechseln
+cd ../
+
+
+#-------------------------
+# Bau das Netzwerk
+docker network create traefik
+
+#-------------------------
+# Bau des WebpageImage
+docker build -t webpageimage:0.1 .
+
+#-------------------------
+# Starte Docker compose
+docker-compose up -d
