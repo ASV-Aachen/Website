3d3c66c2f6a5cbdd7759c4aecc21f8833edb4735
Christian - Darker97 <christian@baltzer.de>
2021-01-28 15:34:07 +0100

    Aufräumen

--- Webpage/Django/urls.py
+++ Webpage/Django/urls.py
@@ -19,5 +19,4 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('FrontPage.urls')),
-    path('keycloak/', include('django_keycloak.urls')),
 ]
