97c1521ff228aeb08d380b189b9607d7ee02fed4
Christian Wolff-Jesse <76421825+henk2905@users.noreply.github.com>
2021-02-07 15:31:30 +0100

    aufgeräumt

--- Webpage/Mitglieder/urls.py
+++ Webpage/Mitglieder/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
-from django.urls import path, include
+from django.urls import path
 from . import views
 
 app_name: "Mitglieder"
@@ -24,6 +24,5 @@ urlpatterns = [
     path('Mitgliederverzeichnis', views.Migliederverzeichnis, name="Mitgliedererzeichnis"),
     path('User', views.EinzelNutzer, name="User"),
     path('Einstellungen', views.Einstellungen, name="Einstellungen"),
-    path('accounts/', include('django.contrib.auth.urls')),
 
 ]
