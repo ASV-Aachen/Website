47d53cb10aa69bbf0de72f5b294d63e27bf8a03f
Axel Schmidt <schmidt.axel.91@gmail.com>
2021-02-07 02:27:24 +0100

    tinymce editor

--- Webpage/Django/urls.py
+++ Webpage/Django/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from filebrowser.sites import site
 
 urlpatterns = [
+    path('admin/filebrowser/', site.urls),
     path('admin/', admin.site.urls),
+    path('tinymce/', include('tinymce.urls')),
     path('', include('FrontPage.urls')),
 ]
