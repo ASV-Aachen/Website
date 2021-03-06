72924f3a869ad9ec51a6ff3be2f3e9d8979fef2a
Christian - Darker97 <christian@baltzer.de>
2021-03-06 11:05:23 +0100

    Added Backend Cropping for user images

--- Webpage/member/models.py
+++ Webpage/member/models.py
@@ -1,3 +1,4 @@
+from PIL import Image
 from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
@@ -45,6 +46,15 @@ class profile(models.Model):
     def __str__(self):
         return self.user.first_name + " " + self.user.last_name
 
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        img = Image.open(self.profile_image)
+
+        # images => 166*233
+        if (img.width < 166 or img.height < 233):
+            output = (166,233)
+            img.thumbnail(output)
+            img.save(self.profile_image)
 
 
 class position_in_the_club(models.Model):
