cf7f8f6917e15459a7d9e35f4a4d8472add41c12
Christian - Darker97 <christian@baltzer.de>
2021-03-07 11:50:28 +0100

    Test

--- Webpage/blog/forms.py
+++ Webpage/blog/forms.py
@@ -10,10 +10,6 @@ from .models import blogPost
 
 
 class newBlogEntry(ModelForm):
-    text = forms.CharField(widget=TinyMCE(attrs={'cols': 80,
-                                                 'rows':50,
-                                                 'class': 'form-control'}))
-
     class Meta:
         model = blogPost
         fields = ['titel', 'text']
