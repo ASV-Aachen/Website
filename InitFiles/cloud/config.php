fc87a8c28579f0ca359108aade057805c9156a63
Christian Fimmers <christian.fimmers@rwth-aachen.de>
2021-03-23 22:48:00 +0100

    basic setup

---
+++ InitFiles/cloud/config.php
@@ -0,0 +1,16 @@
+<?php
+$CONFIG = array (
+  'overwritewebroot' => '/cloud',
+  'instanceid' => 'ocbrlbtotthi',
+  'passwordsalt' => 'JgGmkAkaaOuGejlisyndEjXnm+8KxB',
+  'secret' => '/iujrG/u4ynmu0GFGh95LQnASA+JeqIxvD/s+9vCUInNVGdo',
+  'trusted_domains' => 
+  array (
+    0 => '192.168.178.20:11100',
+  ),
+  'datadirectory' => '/var/www/html/data',
+  'dbtype' => 'sqlite3',
+  'version' => '21.0.0.18',
+  'overwrite.cli.url' => 'http://192.168.178.20:11100/cloud',
+  'installed' => true,
+);
