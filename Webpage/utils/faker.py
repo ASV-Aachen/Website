ebc534035061ac1a3bf7ece248a06a66e7770bdd
Christian - Darker97 <christian@baltzer.de>
2021-03-26 22:00:37 +0100

    Init Fake Functions

---
+++ Webpage/utils/faker.py
@@ -0,0 +1,19 @@
+
+'''
+Erstellt die gewünschte Anzahl an fakeNews.
+fakeNews sind dabei News müssen nicht zwangsläufig einen Sinn haben.
+
+@return Array, bestehend aus Objekten mit Text, Titel
+'''
+def fakeNews(Anzahl: int) -> []:
+
+    pass
+
+'''
+Erstellt die gewünschte Anzahl an Fake Nutzern. 
+
+@return Array aus Objecten -> username, vorname, nachname, country, hometown, EMail, HandyNummer
+'''
+def fakeNutzer(Anzahl: int) -> []:
+    # TODO
+    pass
