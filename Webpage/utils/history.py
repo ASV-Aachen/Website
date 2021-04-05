cb1f265408944bdcff9889694c70bc5e188c0c48
Christian - Darker97 <christian@baltzer.de>
2021-04-06 00:29:27 +0200

    init

---
+++ Webpage/utils/history.py
@@ -0,0 +1,10 @@
+
+'''
+delete history older than one Month and if there are more than 5 from Models
+
+Models:     blogPost -> blogPostHistory
+            infoPage -> infoPageHistory
+'''
+def deleteOlderHistory():
+    #TODO
+    pass
