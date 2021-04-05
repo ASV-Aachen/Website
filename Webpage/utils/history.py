b21b35517e0e2ebd952862c4c0f99d1d5a5c775e
Christian - Darker97 <christian@baltzer.de>
2021-04-06 00:42:55 +0200

    added function that will delete all history older than 30 days if there is more than 7 elements

--- Webpage/utils/history.py
+++ Webpage/utils/history.py
@@ -1,10 +1,26 @@
+import datetime
+
+from blog.models import blogPost
+from web.models import infoPage
 
 '''
-delete history older than one Month and if there are more than 5 from Models
+delete history older than one Month and if there are more than 7 from Models
 
 Models:     blogPost -> blogPostHistory
             infoPage -> infoPageHistory
 '''
 def deleteOlderHistory():
-    #TODO
-    pass
+    for alleBlogPosts in blogPost.objects.all():
+        for hist in alleBlogPosts.history.all().order_by('-id'):
+            if alleBlogPosts.history.all().count() > 7:
+                break
+            if (hist.date < datetime.now()- datetime.timedelta(days=30)):
+                hist.delete()
+
+    for allinfoPage in infoPage.objects.all():
+        for hist in allinfoPage.history.all().order_by('-id'):
+            if allinfoPage.history.all().count() > 7:
+                break
+            if (hist.date < datetime.now()- datetime.timedelta(days=30)):
+                hist.delete()
+
