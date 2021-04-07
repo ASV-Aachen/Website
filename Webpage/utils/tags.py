8b63c4f15ea283e860fe0e384e53dcde8bd76a20
Christian - Darker97 <christian@baltzer.de>
2021-04-07 21:35:37 +0200

    Added Tags for the Models

--- Webpage/utils/tags.py
+++ Webpage/utils/tags.py
@@ -1,5 +1,7 @@
 from django import template
 
+from web.models import frontHeader
+
 '''
 Check for Template
 @example: {% if request.user|has_group:"Editor" %}
@@ -9,3 +11,11 @@ register = template.Library()
 @register.filter('has_group')
 def has_group(user, group_name):
     return user.groups.filter(name=group_name).exists()
+
+@register.simple_tag
+def getLeftFront():
+    return frontHeader.objects.all()[0].left
+
+@register.simple_tag
+def getRightPast():
+    return frontHeader.objects.all()[0].right
