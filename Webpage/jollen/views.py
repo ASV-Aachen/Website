d27b49dddd4348d3ac30843651212bc53ba75844
Christian - Darker97 <christian@baltzer.de>
2021-04-27 20:39:47 +0200

    added Views

--- Webpage/jollen/views.py
+++ Webpage/jollen/views.py
@@ -7,7 +7,8 @@ from django.core.paginator import Paginator
 from django.http import HttpResponse
 from django.shortcuts import render, redirect, get_object_or_404
 
-
+from .forms import *
+from .models import *
 
 # Create your views here.
 
@@ -16,19 +17,58 @@ from django.shortcuts import render, redirect, get_object_or_404
 
 def ubersicht(request):
     # Wenn angemeldet
-
+    jollen = boot.objects.all()
+    if (request.user.is_authenticated):
+        return render(request, "jollen/ubersicht_mitglied.html", context={
+                "Jollen": jollen
+            })
     # wenn nicht
-
-    return render(request, "member/Dashboard.html")
+    else:
+        return render(request, "jollen/ubersicht_gast.html", context={
+                "Jollen": jollen
+            })
+    pass
+    
 
 def description(request, name):
-    #TODO
-    pass
+    jolle = get_object_or_404(boot, name=name)
+    return render(request, "jollen/description.html", context={
+                    "Jolle": jolle
+                })
+
 
 @login_required
 def settings_status(request, name):
     # Anpassen vom Status und Nachricht des Bootes
-    #TODO
+    jolle = get_object_or_404(boot, name=name)
+
+    if request.method == "POST":
+        # Eintragen in die DB
+        form = settings_status(request.POST, request.FILES)
+
+        if form.is_valid():
+            form.save(commit=False)
+            form.instance.autor = request.user
+            
+            # neue Historie erstellen
+            newHistory = nachricht_historie(text = form.instance.text, autor = form.instance.autor)
+            newHistory.save()
+            jolle.history.add(newHistory)
+            jolle.save()
+            
+            # abspeichern
+            form.save()
+            
+            return redirect('jollenÜbersicht')
+    else:
+        form = settings_status(instance = jolle.message)
+
+        return render(request, "web/jollen_editor.html", {
+            "jolle": jolle, 
+            "form": form
+            })
+
+
     pass
 
 @login_required
@@ -36,18 +76,28 @@ def settings_status(request, name):
 def settings_description(request, name):
     # Anpassen vom Namen, Beschreibungstext, Bootsclasse und co
     #TODO
+    jolle = get_object_or_404(boot, name=name)
+
+    if request.method == "POST":
+        # Eintragen in die DB
+        form = settings_description(request.POST, request.FILES)
+
+        if form.is_valid():
+            # abspeichern
+            form.save()
+            
+            return redirect('jollenÜbersicht')
+    else:
+        form = settings_description(instance = jolle)
+
+        return render(request, "web/jollen_editor.html", {
+            "jolle": jolle, 
+            "form": form
+            })
     pass
 
 
 '''
-- [ ] View mit Übersicht
-    - Neue Nachrichten und Status nur zeigen wenn man angemeldet ist
-- [ ] Einzelsicht der Boote mit Beschreibungstext
-
-- [ ] View zum bearbeiten...
-    - ... vom Status
-    - ... vom Beschreibungstext
-    - [ ] Jedes Mitglied sollte den Status ändern können
 - [ ] Boote ins MenüObject einfügen
     - [ ] Header-Generierung anpassen
 '''
