dd2db6dd2d27e013cb93bd13785a05c6d56437ae
Christian Wolff-Jesse <76421825+henk2905@users.noreply.github.com>
2021-05-01 12:26:26 +0200

    Back-To-Top Button eingefügt

--- Webpage/static/JS/bulley/custom.js
+++ Webpage/static/JS/bulley/custom.js
@@ -15,3 +15,20 @@ $(document).ready(function () {
         clickDropdown();
     });
 });
+
+$(document).ready(function(){
+	$(window).scroll(function () {
+			if ($(this).scrollTop() > 50) {
+				$('#back-to-top').fadeIn();
+			} else {
+				$('#back-to-top').fadeOut();
+			}
+		});
+		// scroll body to 0px on click
+		$('#back-to-top').click(function () {
+			$('body,html').animate({
+				scrollTop: 0
+			}, 400);
+			return false;
+		});
+});
