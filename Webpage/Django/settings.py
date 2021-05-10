f5dfabd69458d101b50f74ba0dd1e3535afd0a0a
Christian Wolff-Jesse <76421825+henk2905@users.noreply.github.com>
2021-05-10 22:04:40 +0200

    errorPages aktivieren

--- Webpage/Django/settings.py
+++ Webpage/Django/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ["DEBUG"] == "True"
+DEBUG = os.environ["DEBUG"] == "False"
 
 Host = os.environ["Host"]
 ALLOWED_HOSTS = [os.environ["ALLOWED_HOSTS"]]
