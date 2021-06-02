0eaa41afe210d82db4160dbc59782e1d98dca94b
Christian - Darker97 <christian@baltzer.de>
2021-06-02 22:36:41 +0200

    Added Dashboard

--- Webpage/arbeitsstunden/views.py
+++ Webpage/arbeitsstunden/views.py
@@ -3,17 +3,16 @@ from functools import reduce
 from django.db.models import F
 from django.db.models.functions import Coalesce
 from django.shortcuts import render, redirect, get_object_or_404
-
+import hashlib
 # Create your views here.
 from django.views.generic import DeleteView
 
-from arbeitsstunden.forms import ArbeitsstundenausschreibungForm, ProjectForm, ArbeitseinheitForm, \
-    ArbeitsbeteiligungForm
-from arbeitsstunden.models import Arbeitsstundenausschreibung, Arbeitsbeteiligung, Saison, Projekt, Arbeitseinheit
+from .models import *
+from .forms import *
 from django.urls import resolve
 
-from arbeitsstunden.utils import get_aktuelle_saison, check_authentication_redirect_if_fails
 from member.models import profile
+from datetime import datetime
 
 
 '''
@@ -23,8 +22,15 @@ from member.models import profile
 - Übersicht über letzte Eintragungen (Newsfead TODO: #159)
 '''
 def dashboard(request):
-    
-    pass
+    current_account = profile.objects.get(user=request.user).workingHoursAccount
+    last_Works = work.objects.filter(employee = current_account)
+
+    return render(request, template_name="arbeitsstunden/dashboard.html", context={
+        "konto": current_account,
+        "seasons": season.objects.all()[-5],
+        "kostenstelle": costCenter.objects.all(),
+        "last_Work": last_Works
+    })
 
 '''
 Alle Aktiven und geplante Projekte, sortiert nach Zeit, ein und ausklappbar
