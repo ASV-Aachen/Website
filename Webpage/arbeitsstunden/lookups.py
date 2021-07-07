b2c7976723e32c35a029445194862d5ad70276a3
darker97 <christian@baltzer.de>
2021-07-07 23:31:12 +0200

    inserted Lockup fields

---
+++ Webpage/arbeitsstunden/lookups.py
@@ -0,0 +1,28 @@
+# yourapp/lookups.py
+from ajax_select import register, LookupChannel
+from .models import *
+from django.db.models import Q
+
+
+@register('responsible')
+class responsibleLookup(LookupChannel):
+
+    model = User 
+
+    def get_query(self, q, request):
+        # print("TEST")
+        return self.model.objects.filter(Q(first_name__icontains=q) | Q(last_name__icontains=q)).order_by('last_name')[:20]
+
+    def format_item_display(self, item):
+        return u"<span class='tag'>%s %s</span>" % (item.first_name, item.last_name)
+
+@register('employee')
+class employeeLookup(LookupChannel):
+
+    model = account
+
+    def get_query(self, q, request):
+        return self.model.objects.filter(name__icontains=q).order_by('name')[:20]
+    
+    def format_item_display(self, item):
+        return u"<span class='tag'>%s</span>" % item.name
