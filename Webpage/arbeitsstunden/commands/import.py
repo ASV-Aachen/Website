d7ff9373759fdb1384927b7abd61a19f670055fa
Christian Baltzer <35217578+Darker97@users.noreply.github.com>
2021-10-12 12:21:51 +0200

    added function to command

--- Webpage/arbeitsstunden/commands/import.py
+++ Webpage/arbeitsstunden/commands/import.py
@@ -1,20 +1,17 @@
 from django.core.management.base import BaseCommand
 
-class userStruct:
-    pass
+from Webpage.arbeitsstunden.commands.utils.csv import importCSV
 
 
 class Command(BaseCommand):
-    help = "Import a given csv File and i"
+    help = "Import the given csv Files (ONLY FOR GIVEN STRUCT)"
     
     def add_arguments(self, parser: CommandParser) -> None:
         return super().add_arguments(parser)
     
     def handle(self, *args: Any, **options: Any) -> Optional[str]:
         
-        data = self.createData(self.importCSV)
-        
-        self.addToDatabase(data)
+        importCSV()
         
         return super().handle(*args, **options)
     
