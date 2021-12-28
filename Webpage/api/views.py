63fc302275b41c49c0fe4fc33f72d2e0d5148c33
Christian Baltzer <35217578+Darker97@users.noreply.github.com>
2021-12-28 15:01:55 +0100

    init views

--- Webpage/api/views.py
+++ Webpage/api/views.py
@@ -1,3 +1,27 @@
+from django.http.response import JsonResponse
 from django.shortcuts import render
+from Webpage.member import models
+from member.models import profile
+from django.forms.models import model_to_dict
 
 # Create your views here.
+
+def model_to_dict() -> dict:
+    
+    pass
+
+def help(request):
+    pass
+
+def member(request)-> JsonResponse:
+    pass
+
+def singleMember(request, id:str):
+    pass
+
+def addMemeber(request):
+    pass
+
+def updateMember(request):
+    pass
+
