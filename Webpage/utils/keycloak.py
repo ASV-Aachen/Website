534730cc76cdaf2fc86eb0cb9d33834bdf120fc5
Christian Baltzer <35217578+Darker97@users.noreply.github.com>
2022-01-14 15:05:46 +0100

    bug fixes

--- Webpage/utils/keycloak.py
+++ Webpage/utils/keycloak.py
@@ -61,56 +61,63 @@ def update_all_Users():
 
     # Für jeden Nutzer:
     for user in all_Users:
-        # Zieh dir die Daten aus Keycloak
         try:
-            user_id_keycloak = keycloak_admin.get_user_id(username=user.username)
-            keycloak_user = keycloak_admin.get_user(user_id=user_id_keycloak)
+            # Zieh dir die Daten aus Keycloak
+            try:
+                user_id_keycloak = keycloak_admin.get_user_id(username=user.username)
+                keycloak_user = keycloak_admin.get_user(user_id=user_id_keycloak)
+            except:
+                continue
+
+            # keycloak_user:
+            # username      string
+            # email         string
+            # groups        < string > array
+            # firstName     string
+            # lastName      string
+            # realmRoles    < string > array
+
+            user.firstName = keycloak_user['firstName']
+            user.lastName = keycloak_user['lastName']
+            user.email = keycloak_user['email']
+
+            # realm Roles
+            # print("----------------------------")
+
+            # GET /{realm}/users/{id}/groups
+            groups = keycloak_admin.get_user_groups(user_id=user_id_keycloak)
+            # print(groups)
+
+            user.groups.clear()
+            for i in groups:
+                newGroup, created = Group.objects.get_or_create(name=i['name'])
+                user.groups.add(newGroup)
+
+            
+            # GET /{realm}/users/{id}/roles
+            roles = keycloak_admin.get_realm_roles_of_user(user_id=user_id_keycloak)
+            # print(roles)
+
+            userProfile = profile.objects.get(user=user)
+            userProfile.roles.clear()
+            
+            for i in roles:
+                if i == "default-roles-asv":
+                    continue
+                newRole, created = role.objects.get_or_create(titel=i['name'])
+                userProfile.roles.add(newRole)
+
+            user.is_staff = user.groups.filter(name='Admin').exists()
+            user.is_superuser = user.groups.filter(name='Admin').exists()
+                    
+            userProfile.save()
+            user.save()
+            
+            # print(user.groups.filter(name='Admin').exists())
+        
         except:
+            print(user.firstName + user.lastName)
             continue
-
-        # keycloak_user:
-        # username      string
-        # email         string
-        # groups        < string > array
-        # firstName     string
-        # lastName      string
-        # realmRoles    < string > array
-
-        user.firstName = keycloak_user['firstName']
-        user.lastName = keycloak_user['lastName']
-        user.email = keycloak_user['email']
-
-        # realm Roles
-        # print("----------------------------")
-
-        # GET /{realm}/users/{id}/groups
-        groups = keycloak_admin.get_user_groups(user_id=user_id_keycloak)
-        # print(groups)
-
-        user.groups.clear()
-        for i in groups:
-            newGroup, created = Group.objects.get_or_create(name=i['name'])
-            user.groups.add(newGroup)
-
-        
-        # GET /{realm}/users/{id}/roles
-        roles = keycloak_admin.get_realm_roles_of_user(user_id=user_id_keycloak)
-        # print(roles)
-
-        userProfile = profile.objects.get(user=user)
-        userProfile.roles.clear()
-        
-        for i in roles:
-            newRole, created = role.objects.get_or_create(titel=i['name'])
-            userProfile.roles.add(newRole)
-
-        user.is_staff = user.groups.filter(name='Admin').exists()
-        user.is_superuser = user.groups.filter(name='Admin').exists()
-                
-        userProfile.save()
-        user.save()
-        
-        # print(user.groups.filter(name='Admin').exists())
 
     return
 
