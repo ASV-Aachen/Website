19d597bee95b172cb73db6a3b5ededcbb5a90b37
Christian Baltzer <35217578+Darker97@users.noreply.github.com>
2022-04-17 11:51:17 +0200

    Bugfix in Password

--- Webpage/utils/mail/password.py
+++ Webpage/utils/mail/password.py
@@ -3,7 +3,7 @@ import secrets
 
 def createPassword():
     
-    alphabet = string.ascii_letters + string.digits
+    alphabet = string.ascii_letters + string.digits + string.punctuation
     password = ''.join(secrets.choice(alphabet) for i in range(20))
     
     return password
