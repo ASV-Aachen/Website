5762e2ec1867d39d88c0d2a507943d4355b0181d
Christian Baltzer <35217578+Darker97@users.noreply.github.com>
2022-11-08 10:47:15 +0100

    changed Mail

--- Webpage/utils/mail/createMails.py
+++ Webpage/utils/mail/createMails.py
@@ -9,15 +9,14 @@ def createMail(password)->str:
 
     Mit diesem kann man sich bei allen Diensten wie der Arbeitsstundendatenbank, dem Wiki oder der Cloud anmelden.
     Der Nutzername ist dabei die eigene E-Mail-Adresse.
-    Die neue Seite ist aktuell erreichbar unter: https://neu.asv-aachen.de/
+    Die neue Seite ist aktuell erreichbar unter: https://asv-aachen.de/
 
     Dein Passwort lautet: {}
 
-    BITTE BEACHTEN: Die Website hat aktuell noch nicht das endgültige Zertifikat, weswegen es zu einer Warnung im Browser kommt. 
+    BITTE BEACHTEN: Services wie das Wiki oder die Cloud, haben aktuell noch kein vollständiges Zertifikat, weswegen es hier zu ein Warnmeldung im Browser kommt. 
     Dies ist korrekt und wird in Zukunft noch behoben.
 
-    Sollten dir Fehler auffallen, so melde diese gerne an einen der Admins oder trage diese ins folgende Formular ein:
-    https://docs.google.com/forms/d/18ddftQjyX_WWSp2K2tNO9UoosWLea-X2v96AQSA0oCg
+    Bei Fragen oder Anmerkungen, melde dich gerne beim Dev Team unter der Mail Adresse: dev@asv-aachen.de
 
     Viele Grüße,
     Christian Baltzer
